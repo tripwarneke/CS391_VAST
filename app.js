8573b8d6d609ca0d0c8ed705d5ded7047ac3cb9f
Anh Nguyen <anhgau1@yahoo.com>
2012-04-22 22:28:44 -0400

    added gui

--- app.js
+++ app.js
@@ -29,13 +29,16 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
-// Bank Application Routes
-app.get('/', routes.readme);
+app.get('/', routes.home);
 app.get('/create', routes.create);
 app.get('/login', routes.login);
-app.get('/index', routes.login);
+app.get('/index', routes.home);
 app.get('/logout', routes.logout);
-
+app.get('/home', routes.home);
+app.get('/profile', routes.profile);
+app.get('/gpa', routes.gpa);
+app.get('/est', routes.est);
+app.get('/test', routes.test);
 
 //almost forgot to set this back to 3000
 app.listen(8288);
