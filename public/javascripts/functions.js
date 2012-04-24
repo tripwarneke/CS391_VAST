a5edc8334bfe5fd37d0b4bbf997d20499189ab68
Anh Nguyen <anhgau1@yahoo.com>
2012-04-24 17:28:51 -0400

    nothing

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,2 +1,35 @@
 //place functions for client side interactions and checks here
 
+var interval_id;
+
+//set interval to 3 seconds
+var start_polling = function () {
+	interval_id = setInterval(get_msg, 3000);
+};
+
+var stop_polling = function () {
+	if (interval_id) {
+		clearInterval(interval_id);
+	}
+};
+
+$(function () {
+	get_data();
+	//start_polling();// will poll every 3 seconds
+});
+
+
+var get_data = function () {
+	var req = $.ajax({
+		type: 'GET',
+		url : '/get_data'
+	});	
+	req.done(function (data) {
+		user = data;
+		//console.log('received data: ' + data.msg);
+		$('#name').text(user.u_name);
+		$('#school').text(user.u_school);
+		
+
+	});
+};
